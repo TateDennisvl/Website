9382774b86a9397c532ed7665a236aed4ab60134
TateDennisvl <169009958+TateDennisvl@users.noreply.github.com>
2024-05-06 15:44:33 -0700

    e

--- Games/game.js
+++ Games/game.js
@@ -1,10 +1,7 @@
-document.getElementById('fullscreen-button').addEventListener('click', function() {
-    var frame = document.getElementById('frame');
-    if (frame.requestFullscreen) {
-        frame.requestFullscreen();
-    } else if (frame.webkitRequestFullscreen) { 
-        frame.webkitRequestFullscreen(); // Safari
-    } else if (frame.msRequestFullscreen) { 
-        frame.msRequestFullscreen(); // IE11
+function toggleFullscreen() {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
     }
-});
+  }
